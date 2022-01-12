ed4515fc359d3d0f3afada75e13b2a270129e28e
unknown <mamadoumoustaphadiallo05@gmail.com>
2022-01-12 17:05:27 +0100

    commit du fichier Category.java

---
+++ src/main/java/com/sene/entities/Category.java
@@ -0,0 +1,13 @@
+package com.sene.entities;
+
+public class Category{
+    private int id;
+    private String libelle;
+
+    public Category(){}
+
+    public Category(int id,String libelle){
+        this.id=id;
+        this.libelle=libelle;
+    }
+}

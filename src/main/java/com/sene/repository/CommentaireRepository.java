b1dfec36e463798a5be371a19ee5cde467846f53
unknown <mamadoumoustaphadiallo05@gmail.com>
2022-04-01 17:21:10 +0200

    teste de commite

---
+++ src/main/java/com/sene/repository/CommentaireRepository.java
@@ -0,0 +1,7 @@
+package com.sene.repository;
+
+import com.sene.entities.Commentaire;
+
+public interface CommentaireRepository {
+    public Commentaire add(Commentaire c);
+}
---
+++ src/main/java/com/sene/repository/jdbc/CommantaireJdbcRepository.java
@@ -0,0 +1,11 @@
+package com.sene.repository.jdbc;
+
+import com.sene.entities.Commentaire;
+import com.sene.repository.CommentaireRepository;
+
+public class CommantaireJdbcRepository implements CommentaireRepository {
+    @Override
+    public Commentaire add(Commentaire c) {
+        return null;
+    }
+}
